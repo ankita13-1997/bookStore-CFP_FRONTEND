aa6ecadb15534321e278ee1922dfc42c06ee9020
Parhi <ankita.parhi@siemens.com>
2022-05-30 21:52:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      title : "Hello World"
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+      </div>
+    );
+  }
+}
+export default App;
